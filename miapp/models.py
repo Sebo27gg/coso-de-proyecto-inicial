d7c220d3823700db3974083c658bcce61db57318
Sanguche-de-potito <mardonespenabenjamin@gmail.com>
2025-10-12 03:00:46 -0300

    modelo de alergia

--- miapp/models.py
+++ miapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Allergy(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    users = models.ManyToManyField(User, blank=True)
+    def __str__(self):
+        return self.name
